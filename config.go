1cebcd385cf00d258fc232e9ed51b11617c49193
Mark Newman <mark.newman@r3labs.io>
2017-09-08 11:51:56 +0100

    Increase Postgres wait timer

--- config.go
+++ config.go
@@ -77,11 +77,11 @@ func (c *Config) Postgres(table string) *gorm.DB {
 		uri := fmt.Sprintf("%s/%s?sslmode=disable", pgCfg["url"], table)
 		c.postgres, err = gorm.Open("postgres", uri)
 		if err != nil {
-			log.Println("Unsuccesful connection to postgres '" + uri + "'. Retrying in 20 seconds ...")
-			time.Sleep(time.Second * 20)
+			log.Println("Unsuccesful connection to postgres '" + uri + "'. Retrying in 30 seconds ...")
+			time.Sleep(time.Second * 30)
 			continue
 		} else {
-			time.Sleep(time.Second * 20)
+			time.Sleep(time.Second * 30)
 		}
 		log.Println("Successfully connected to postgres on '" + uri + "'")
 	}
